9dcaf26aa404dfc97a16fa32196ff541ceb43b89
omarjoa88 <omarjoa@gmail.com>
2025-10-17 12:12:34 -0500

    Base del html y Css

--- codigos.py
+++ codigos.py
@@ -4,3 +4,21 @@ comandos git
 git config --global user.name "omarjoa88"
 git config --global user.email "omarjoa@gmail.com"
 
+# 1. Abres la carpeta de tu sitio (por ejemplo: portafolio)
+cd C:\Users\Omar\Documents\portafolio
+
+# 2. Inicializas Git
+git init     
+git remote add origin https://github.com/omarjoa88/Educacion-y-Analisis.git
+git branch -M main
+git remote -v
+
+# cargar 
+git add .    
+git commit -m "Subo mi proyecto local a GitHub"
+git push -u origin main
+siempre que suba tengo que poner 
+git pull origin main --rebase
+para actualizar los archivos 
+git push -u origin main
+'''
